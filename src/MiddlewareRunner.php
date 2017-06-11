7e825a3e5ab65c43a0e9396d0aac62e932a897dc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-06-11 12:28:42 +0200

    Experimental annotation based middleware ordering

--- src/MiddlewareRunner.php
+++ src/MiddlewareRunner.php
@@ -2,11 +2,14 @@
 
 namespace ApiClients\Foundation\Middleware;
 
+use ApiClients\Foundation\Middleware\Annotation\Priority as PriorityAnnotation;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use React\Promise\CancellablePromiseInterface;
 use Throwable;
 use function React\Promise\reject;
+use ReflectionMethod;
 use function React\Promise\resolve;
 
 final class MiddlewareRunner
@@ -22,6 +25,11 @@ final class MiddlewareRunner
     private $middlewares;
 
     /**
+     * @var AnnotationReader
+     */
+    private $annotationReader;
+
+    /**
      * MiddlewareRunner constructor.
      * @param array $options
      * @param MiddlewareInterface[] $middlewares
@@ -29,22 +37,38 @@ final class MiddlewareRunner
     public function __construct(array $options, MiddlewareInterface ...$middlewares)
     {
         $this->options = $options;
-        $this->middlewares = $this->orderMiddlewares(...$middlewares);
+        $this->middlewares = $middlewares;
+        $this->annotationReader = new AnnotationReader();
     }
 
     /**
      * Sort the middlewares by priority
      *
+     * @param string $method
      * @param MiddlewareInterface[] $middlewares
      * @return array
      */
-    protected function orderMiddlewares(MiddlewareInterface ...$middlewares): array
+    protected function orderMiddlewares(string $method, MiddlewareInterface ...$middlewares): array
     {
-        usort($middlewares, function (MiddlewareInterface $left, MiddlewareInterface $right) {
-            return $right->priority() <=> $left->priority();
+        usort($middlewares, function (MiddlewareInterface $left, MiddlewareInterface $right) use ($method) {
+            return $this->getPriority($method, $right) <=> $this->getPriority($method, $left);
         });
 
         return $middlewares;
+    }
+
+    private function getPriority(string $method, MiddlewareInterface $middleware): int
+    {
+        $methodReflection = new ReflectionMethod($middleware, $method);
+        $annotation = $this->annotationReader->getMethodAnnotation($methodReflection, PriorityAnnotation::class);
+
+        if ($annotation !== null &&
+            get_class($annotation) === PriorityAnnotation::class
+        ) {
+            return $annotation->property;
+        }
+
+        return $middleware->priority();
     }
 
     /**
@@ -56,7 +80,10 @@ final class MiddlewareRunner
     ): CancellablePromiseInterface {
         $promise = resolve($request);
 
-        foreach ($this->middlewares as $middleware) {
+        $middlewares = $this->middlewares;
+        $middlewares = $this->orderMiddlewares('pre', ...$middlewares);
+
+        foreach ($middlewares as $middleware) {
             $requestMiddleware = $middleware;
             $promise = $promise->then(function (RequestInterface $request) use ($requestMiddleware) {
                 return $requestMiddleware->pre($request, $this->options);
@@ -75,9 +102,11 @@ final class MiddlewareRunner
     ): CancellablePromiseInterface {
         $promise = resolve($response);
 
-        $this->middlewares = array_reverse($this->middlewares);
+        $middlewares = $this->middlewares;
+        $middlewares = $this->orderMiddlewares('post', ...$middlewares);
+        $middlewares = array_reverse($middlewares);
 
-        foreach ($this->middlewares as $middleware) {
+        foreach ($middlewares as $middleware) {
             $responseMiddleware = $middleware;
             $promise = $promise->then(function (ResponseInterface $response) use ($responseMiddleware) {
                 return $responseMiddleware->post($response, $this->options);
@@ -97,9 +126,11 @@ final class MiddlewareRunner
 
         $promise = reject($throwable);
 
-        $this->middlewares = array_reverse($this->middlewares);
+        $middlewares = $this->middlewares;
+        $middlewares = $this->orderMiddlewares('error', ...$middlewares);
+        $middlewares = array_reverse($middlewares);
 
-        foreach ($this->middlewares as $middleware) {
+        foreach ($middlewares as $middleware) {
             $errorMiddleware = $middleware;
             $promise = $promise->then(null, function (Throwable $throwable) use ($errorMiddleware) {
                 return reject($errorMiddleware->error($throwable, $this->options));
